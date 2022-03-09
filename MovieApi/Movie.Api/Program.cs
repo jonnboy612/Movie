6a08c222cf59f4be4787c4e0d0b043b5fc975533
Jonathan <jonothan.lee3@gmail.com>
2022-03-08 19:30:21 -0500

    edited program.cs to add dbcontext

--- MovieApi/Movie.Api/Program.cs
+++ MovieApi/Movie.Api/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Movie.Data.DataAccess;
+using Movie.Data.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MovieContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
